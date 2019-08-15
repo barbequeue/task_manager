151382ea4dbc7a86d6a7ec6d37904ba99cf7a6cc
Stas Bulanov <p99kab00@gmail.com>
2019-08-15 22:31:43 +0300

    Bug fixes and improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
 
-  resources :projects do
+  resources :projects, path: '/' do
     resources :tasks do
       member do
         patch 'checkout'
